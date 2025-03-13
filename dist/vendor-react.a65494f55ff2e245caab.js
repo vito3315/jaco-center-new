21613347f3202bb0e5c979a2b679c8ad48ed8046
vito3315 <marusiaqwerti@yandex.ru>
2025-03-13 21:08:18 +0400

    111

---
+++ dist/vendor-react.a65494f55ff2e245caab.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_ssr=self.webpackChunkreact_ssr||[]).push([[967],{6540:(s,e,r)=>{s.exports=r(5287)}}]);
+//# sourceMappingURL=vendor-react.a65494f55ff2e245caab.js.map
